f7d7e0d3b828dc7589b7dc38c9c9c6901291a7d9
lucas arroyo <luksarroyo_cba@hoymail.com>
2023-04-07 16:52:58 -0300

    creacionProductos

---
+++ src/productsMock.js
@@ -0,0 +1,42 @@
+export const products = [
+    {
+        id: 1,
+        title: "ZAPATILLAS SALOMON ALPHACROSS 3 HOMBRE",
+        price: 32999,
+        stock: 5,
+        description: "Zapatillas ultra trail",
+        category: "trail Run",
+        img: "https://res.cloudinary.com/dhk4b7fdq/image/upload/v1680896639/products/Zapatillas-Salomon-Alphacross-3-Trail-Running-Hombre-Blue-Steel-Lunar-Rock-Black-415997-1_b6t2ev.jpg",
+
+    },
+    {
+        id: 2,
+        title: "ZAPATILLAS SALOMON SUPERCROSS 3 TRAIL RUNNING HOMBRE EBONY LUNAR ROCK" ,
+        price: 36999,
+        stock: 8 ,
+        description: "Terrenos embarrados o blandos",
+        category:"trail Run" ,
+        img:"https://res.cloudinary.com/dhk4b7fdq/image/upload/v1680896810/products/Zapatillas-Salomon-Supercross-3-Trail-Running-Hombre-Ebony-Lunar-Rock-414504_tpv54v.jpg" ,
+
+    },
+    {
+        id: 3 ,
+        title:"ZAPATILLAS SAUCONY ENDORPHIN TRAIL HOMBRE",
+        price: 58990 ,
+        stock:3,
+        description: "l Endorphin Trail lleva nuestra revolucionaria tecnología SPEEDROLL lista para la carrera fuera de la carretera por primera vez, brindando una experiencia de carrera como ninguna otra",
+        category: "trail Run" ,
+        img: "https://res.cloudinary.com/dhk4b7fdq/image/upload/v1680896976/products/Zapatillas-Saucony-Endorphin-Trail-Trail-Running-Hombre-Blue-Raz-Spice-S20647-25_tf29vd.jpg",
+
+    },
+    {
+        id: ,
+        title: ,
+        price: ,
+        stock: ,
+        description: ,
+        category: ,
+        img: ,
+
+    }
+]
